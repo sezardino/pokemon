dfdf0610408d6a7758493158c622d02956b13029
Edward Arechwa <e.arechwa@nebucode.com>
2023-05-07 11:11:32 +0200

    ft: add query client

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { queryClient } from "./libs/query";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <div className="container">
-      <h1>React App</h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="container">
+        <h1>React App</h1>
+      </div>
+    </QueryClientProvider>
   </React.StrictMode>
 );
